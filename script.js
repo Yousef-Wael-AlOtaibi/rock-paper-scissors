5ce66bf34706c5d13c2c50466944e3fc147fb148
Yousef-Wael-AlOtaibi <ywalotaibi2010@gmail.com>
2025-10-25 09:34:41 +0300

    Add playGame() function to call playRound 5 times

--- script.js
+++ script.js
@@ -7,20 +7,29 @@ function getComputerChoice(){
 function getHumanChoice(){
     return prompt('Enter your choice between rock, paper, and scissors.').toLowerCase();
 }
-let humanScore = 0;
-let computerScore = 0;
-function playRound(humanChoice,computerChoice){
-    if(humanChoice === computerChoice){
-        console.log(`It's a tie! ${humanChoice} can't hurt ${computerChoice}.`)
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice,computerChoice){
+        if(humanChoice === computerChoice){
+            console.log(`It's a tie! ${humanChoice} can't hurt ${computerChoice}.`)
+        }
+        else if(humanChoice === 'rock' && computerChoice === 'scissors' ||
+            humanChoice === 'scissors' && computerChoice === 'paper' ||
+            humanChoice === 'paper' && computerChoice === 'rock'){
+            humanScore++;
+            console.log(`Human wins! ${humanChoice} beats ${computerChoice}.`);
+        }
+        else{
+            computerScore++;
+            console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`)
+        }
     }
-    if(humanChoice === 'rock' && computerChoice === 'scissors' ||
-         humanChoice === 'scissors' && computerChoice === 'paper' ||
-          humanChoice === 'paper' && computerChoice === 'rock'){
-        humanScore++;
-        console.log(`Human wins! ${humanChoice} beats ${computerChoice}.`);
-    }
-    else{
-        computerScore++;
-        console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`)
-    }
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    console.log(`Human score: ${humanScore}, computer score: ${computerScore}, ties: ${5-(humanScore+computerScore)}`)
 }
+playGame();
