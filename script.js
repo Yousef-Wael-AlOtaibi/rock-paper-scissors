4b49604d0fb1544c14a2fed0f3d9ed70b60eaba0
Yousef-Wael-AlOtaibi <ywalotaibi2010@gmail.com>
2025-10-25 09:21:40 +0300

    Add playRound function and make input usable
    The playRound func takes the choices and logs the
    winner, also incrementing the winner's score.
    Also updated the getHumanChoice func to return
    a lowerCase version of the input to make sure our
    conditions pass.

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ function getComputerChoice(){
     if(randomNumber === 2)return 'scissors';
 }
 function getHumanChoice(){
-    return prompt('Enter your choice between rock, paper, and scissors.');
+    return prompt('Enter your choice between rock, paper, and scissors.').toLowerCase();
 }
 let humanScore = 0;
 let computerScore = 0;
+function playRound(humanChoice,computerChoice){
+    if(humanChoice === computerChoice){
+        console.log(`It's a tie! ${humanChoice} can't hurt ${computerChoice}.`)
+    }
+    if(humanChoice === 'rock' && computerChoice === 'scissors' ||
+         humanChoice === 'scissors' && computerChoice === 'paper' ||
+          humanChoice === 'paper' && computerChoice === 'rock'){
+        humanScore++;
+        console.log(`Human wins! ${humanChoice} beats ${computerChoice}.`);
+    }
+    else{
+        computerScore++;
+        console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`)
+    }
+}
