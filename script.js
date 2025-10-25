f2ee00aa87f894a29fe909f9a1af30fb59ae7af1
Yousef-Wael-AlOtaibi <ywalotaibi2010@gmail.com>
2025-10-25 09:03:29 +0300

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello World!')
+function getComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3);
+    if(randomNumber === 0)return 'rock';
+    if(randomNumber === 1)return 'paper';
+    if(randomNumber === 2)return 'scissors';
+}
